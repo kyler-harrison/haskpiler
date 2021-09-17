ebbe4906c0a88b2ed6271ce702f71da44fdb657f
kyler-harrison <harri828@purdue.edu>
2021-09-16 22:57:14 -0400

    getting better, but need to learn how to IO the right way

--- scanner/scan.hs
+++ scanner/scan.hs
@@ -1,4 +1,13 @@
-data TokenItem = Token Integer | IntVal Integer
+import System.IO
 
+-- TODO figure out what the heck a do block does and how to handle IO
+readChar :: Handle -> IO String
+readChar file = line
+    where line = hGetLine file
+
+main :: IO ()
 main = do
-    putStrLn("what")
+    file <- openFile "urmom.txt" ReadMode
+    line <- readChar file
+    putStrLn line
+    hClose file
