4e4f943d744a700dfd881211a0cebf9cf4691d0a
kyler-harrison <harri828@purdue.edu>
2021-10-14 01:06:47 -0400

    created function to build integer from strings, not sure if it's working correctly, not reading in int values into Tokens correctly yet

--- scanner/scan.hs
+++ scanner/scan.hs
@@ -1,5 +1,5 @@
 import System.IO
-import Data.Char (isDigit)
+import Data.Char (isDigit, digitToInt)
 
 -- TODO
 -- [x] read in file char by char
@@ -9,23 +9,32 @@ import Data.Char (isDigit)
 -- [] put data in Token
 
 data TokenSymbol = PLUS | MINUS | STAR | SLASH | INT_LIT | NONE deriving (Show)
-data Token = Token {tokenVal :: TokenSymbol, intVal :: Int} deriving (Show)
+data Token = Token {tokenVal :: TokenSymbol, intVal :: String} deriving (Show)
 
 -- given a character from file, assign a Token list (of none or a singular Token) if a TokenSymbol match
-processChar :: Char -> [Token]
+processChar :: String -> [Token]
 processChar char 
-  | char == '+' = [Token {tokenVal = PLUS, intVal = 0}]
-  | char == '-' = [Token {tokenVal = MINUS, intVal = 0}]
-  | char == '*' = [Token {tokenVal = STAR, intVal = 0}]
-  | char == '/' = [Token {tokenVal = SLASH, intVal = 0}]
+  | char == "+" = [Token {tokenVal = PLUS, intVal = "0"}]
+  | char == "-" = [Token {tokenVal = MINUS, intVal = "0"}]
+  | char == "*" = [Token {tokenVal = STAR, intVal = "0"}]
+  | char == "/" = [Token {tokenVal = SLASH, intVal = "0"}]
+  | all isDigit char = [Token {tokenVal = INT_LIT, intVal = char}]
   | otherwise = [] 
+
+-- build an integer string recursively (using list concatenation instead of math)
+buildInt :: String -> String 
+buildInt [] = []
+buildInt (x:xs)
+  | isDigit x = [x] ++ buildInt xs
+  | otherwise = []
 
 -- given contents of file String, assign list of Tokens from characters
 readTokens :: String -> [Token]
 readTokens [] = []
 readTokens (char:char_arr) 
-  | isDigit char = [Token {tokenVal = NONE, intVal = 7}] ++ readTokens char_arr
-  | otherwise = processChar char ++ readTokens char_arr
+  | isDigit char = processChar intStr ++ readTokens (drop (length intStr - 1) char_arr)
+  | otherwise = processChar [char] ++ readTokens char_arr
+  where intStr = buildInt char_arr 
 
 main = do
     file <- openFile "urmom.txt" ReadMode
