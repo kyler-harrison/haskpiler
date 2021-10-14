c5f4aee8c656fad30f169aa4104478867b48aa96
kyler-harrison <harri828@purdue.edu>
2021-10-14 01:30:06 -0400

    cleaned up names, simple scanner pretty much done now

--- scanner/scan.hs
+++ scanner/scan.hs
@@ -6,7 +6,7 @@ import Data.Char (isDigit, digitToInt)
 -- [x] check if char is a TokenSymbol
 -- [x] check if char is digit
 --    [x] build up an integer
--- [] put data in Token
+-- [x] put data in Token
 
 data TokenSymbol = PLUS | MINUS | STAR | SLASH | INT_LIT | NONE deriving (Show)
 data Token = Token {tokenVal :: TokenSymbol, intVal :: Int} deriving (Show)
@@ -16,14 +16,16 @@ buildInt :: String -> Int
 buildInt [] = 0
 buildInt (x:xs) = (digitToInt x) * (10 ^ (length xs)) + buildInt xs
 
--- given a character from file, assign a Token list (of none or a singular Token) if a TokenSymbol match
-processChar :: String -> [Token]
-processChar char 
-  | char == "+" = [Token {tokenVal = PLUS, intVal = 0}]
-  | char == "-" = [Token {tokenVal = MINUS, intVal = 0}]
-  | char == "*" = [Token {tokenVal = STAR, intVal = 0}]
-  | char == "/" = [Token {tokenVal = SLASH, intVal = 0}]
-  | all isDigit char = [Token {tokenVal = INT_LIT, intVal = (buildInt char)}]
+{- given a str (might be single byte or entire literal value) from file, assign a 
+   Token list (of none or a singular Token) if a TokenSymbol match
+-}
+processStr :: String -> [Token]
+processStr str 
+  | str == "+" = [Token {tokenVal = PLUS, intVal = 0}]
+  | str == "-" = [Token {tokenVal = MINUS, intVal = 0}]
+  | str == "*" = [Token {tokenVal = STAR, intVal = 0}]
+  | str == "/" = [Token {tokenVal = SLASH, intVal = 0}]
+  | all isDigit str = [Token {tokenVal = INT_LIT, intVal = (buildInt str)}]
   | otherwise = [] 
 
 -- build an integer string until next character isnt a digit (using list concatenation)
@@ -37,8 +39,8 @@ buildIntStr (x:xs)
 readTokens :: String -> [Token]
 readTokens [] = []
 readTokens (char:char_arr) 
-  | isDigit char = processChar intStr ++ readTokens (drop (length intStr - 1) char_arr)
-  | otherwise = processChar [char] ++ readTokens char_arr
+  | isDigit char = processStr intStr ++ readTokens (drop (length intStr - 1) char_arr)
+  | otherwise = processStr [char] ++ readTokens char_arr
   where intStr = buildIntStr (char:char_arr)
 
 main = do
@@ -46,5 +48,4 @@ main = do
     contents <- hGetContents file
     let res = readTokens contents
     print res
-    --print (length res)
     hClose file
