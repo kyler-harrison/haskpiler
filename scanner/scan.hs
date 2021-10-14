cd4a7b9823ff71fe28afc223ce8509fd84fdce1d
kyler-harrison <harri828@purdue.edu>
2021-10-14 00:03:10 -0400

    a workaround for the chars that dont matter, will probably remove later, but gets all TokenSymbols

--- scanner/scan.hs
+++ scanner/scan.hs
@@ -11,19 +11,19 @@ import Data.Char (isDigit)
 data TokenSymbol = PLUS | MINUS | STAR | SLASH | INT_LIT | NONE deriving (Show)
 data Token = Token {tokenVal :: TokenSymbol, intVal :: Int} deriving (Show)
 
--- given a character from file, assign a Token list if a TokenSymbol match
-processChar :: Char -> Token
+-- given a character from file, assign a Token list (of none or a singular Token) if a TokenSymbol match
+processChar :: Char -> [Token]
 processChar char 
-  | char == '+' = Token {tokenVal = PLUS, intVal = 0}
-  | char == '-' = Token {tokenVal = MINUS, intVal = 0}
-  | char == '*' = Token {tokenVal = STAR, intVal = 0}
-  | char == '/' = Token {tokenVal = SLASH, intVal = 0}
-  | otherwise = Token {tokenVal = NONE, intVal = 0}
+  | char == '+' = [Token {tokenVal = PLUS, intVal = 0}]
+  | char == '-' = [Token {tokenVal = MINUS, intVal = 0}]
+  | char == '*' = [Token {tokenVal = STAR, intVal = 0}]
+  | char == '/' = [Token {tokenVal = SLASH, intVal = 0}]
+  | otherwise = [] 
 
 -- given contents of file String, assign list of Tokens from characters
 readTokens :: String -> [Token]
 readTokens [] = []
-readTokens (char:char_arr) = [processChar char] ++ readTokens char_arr
+readTokens (char:char_arr) = processChar char ++ readTokens char_arr
 
 main = do
     file <- openFile "urmom.txt" ReadMode
