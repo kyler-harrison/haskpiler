a462d7221839e44a0a2cd7e52e1ace2b28760760
kyler-harrison <harri828@purdue.edu>
2021-10-13 23:57:58 -0400

    conditionals to create Tokens in processChar, think i need another function handle invalid chars

--- scanner/scan.hs
+++ scanner/scan.hs
@@ -8,21 +8,27 @@ import Data.Char (isDigit)
 --    [] build up an integer
 -- [] put data in Token
 
-data TokenSymbol = PLUS | MINUS deriving (Show)
+data TokenSymbol = PLUS | MINUS | STAR | SLASH | INT_LIT | NONE deriving (Show)
 data Token = Token {tokenVal :: TokenSymbol, intVal :: Int} deriving (Show)
 
 -- given a character from file, assign a Token list if a TokenSymbol match
-processChar :: Char -> [Token]
-processChar char = [Token {tokenVal = PLUS, intVal = 2}]
+processChar :: Char -> Token
+processChar char 
+  | char == '+' = Token {tokenVal = PLUS, intVal = 0}
+  | char == '-' = Token {tokenVal = MINUS, intVal = 0}
+  | char == '*' = Token {tokenVal = STAR, intVal = 0}
+  | char == '/' = Token {tokenVal = SLASH, intVal = 0}
+  | otherwise = Token {tokenVal = NONE, intVal = 0}
 
 -- given contents of file String, assign list of Tokens from characters
 readTokens :: String -> [Token]
 readTokens [] = []
-readTokens (char:char_arr) = processChar char ++ readTokens char_arr
+readTokens (char:char_arr) = [processChar char] ++ readTokens char_arr
 
 main = do
     file <- openFile "urmom.txt" ReadMode
     contents <- hGetContents file
     let res = readTokens contents
-    print (length res)
+    print res
+    --print (length res)
     hClose file
