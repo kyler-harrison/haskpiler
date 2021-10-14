0124ae0a73862408abdee8037feb4e121f1a12f2
kyler-harrison <harri828@purdue.edu>
2021-10-14 01:26:17 -0400

    changed TokenSymbol intVal back to Int type, wrote buildInt, appears to correctly read in int literals now

--- scanner/scan.hs
+++ scanner/scan.hs
@@ -9,23 +9,28 @@ import Data.Char (isDigit, digitToInt)
 -- [] put data in Token
 
 data TokenSymbol = PLUS | MINUS | STAR | SLASH | INT_LIT | NONE deriving (Show)
-data Token = Token {tokenVal :: TokenSymbol, intVal :: String} deriving (Show)
+data Token = Token {tokenVal :: TokenSymbol, intVal :: Int} deriving (Show)
+
+-- given a string representation of integer, make it an actual Int
+buildInt :: String -> Int
+buildInt [] = 0
+buildInt (x:xs) = (digitToInt x) * (10 ^ (length xs)) + buildInt xs
 
 -- given a character from file, assign a Token list (of none or a singular Token) if a TokenSymbol match
 processChar :: String -> [Token]
 processChar char 
-  | char == "+" = [Token {tokenVal = PLUS, intVal = "0"}]
-  | char == "-" = [Token {tokenVal = MINUS, intVal = "0"}]
-  | char == "*" = [Token {tokenVal = STAR, intVal = "0"}]
-  | char == "/" = [Token {tokenVal = SLASH, intVal = "0"}]
-  | all isDigit char = [Token {tokenVal = INT_LIT, intVal = char}]
+  | char == "+" = [Token {tokenVal = PLUS, intVal = 0}]
+  | char == "-" = [Token {tokenVal = MINUS, intVal = 0}]
+  | char == "*" = [Token {tokenVal = STAR, intVal = 0}]
+  | char == "/" = [Token {tokenVal = SLASH, intVal = 0}]
+  | all isDigit char = [Token {tokenVal = INT_LIT, intVal = (buildInt char)}]
   | otherwise = [] 
 
--- build an integer string recursively (using list concatenation instead of math)
-buildInt :: String -> String 
-buildInt [] = []
-buildInt (x:xs)
-  | isDigit x = [x] ++ buildInt xs
+-- build an integer string until next character isnt a digit (using list concatenation)
+buildIntStr :: String -> String 
+buildIntStr [] = []
+buildIntStr (x:xs)
+  | isDigit x = [x] ++ buildIntStr xs
   | otherwise = []
 
 -- given contents of file String, assign list of Tokens from characters
@@ -34,7 +39,7 @@ readTokens [] = []
 readTokens (char:char_arr) 
   | isDigit char = processChar intStr ++ readTokens (drop (length intStr - 1) char_arr)
   | otherwise = processChar [char] ++ readTokens char_arr
-  where intStr = buildInt (char:char_arr)
+  where intStr = buildIntStr (char:char_arr)
 
 main = do
     file <- openFile "urmom.txt" ReadMode
