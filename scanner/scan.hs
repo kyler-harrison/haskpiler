56a65ade868a9824b0af961d74c911f322868be8
kyler-harrison <harri828@purdue.edu>
2021-10-13 23:43:33 -0400

    basic flow of reading in file by chars, need logic now

--- scanner/scan.hs
+++ scanner/scan.hs
@@ -2,21 +2,27 @@ import System.IO
 import Data.Char (isDigit)
 
 -- TODO
--- [] read in file char by char
+-- [x] read in file char by char
 -- [] check if char is a TokenSymbol
 -- [] check if char is digit
 --    [] build up an integer
 -- [] put data in Token
 
-data Token = Token {tokenVal :: String, intVal :: Int}
-data TokenSymbol = PLUS | MINUS
+data TokenSymbol = PLUS | MINUS deriving (Show)
+data Token = Token {tokenVal :: TokenSymbol, intVal :: Int} deriving (Show)
 
-readChar :: Handle -> IO String
-readChar file = line
-    where line = hGetLine file
+-- given a character from file, assign a Token list if a TokenSymbol match
+processChar :: Char -> [Token]
+processChar char = [Token {tokenVal = PLUS, intVal = 2}]
+
+-- given contents of file String, assign list of Tokens from characters
+readTokens :: String -> [Token]
+readTokens [] = []
+readTokens (char:char_arr) = processChar char ++ readTokens char_arr
 
 main = do
     file <- openFile "urmom.txt" ReadMode
-    line <- readChar file
-    putStrLn line
+    contents <- hGetContents file
+    let res = readTokens contents
+    print (length res)
     hClose file
