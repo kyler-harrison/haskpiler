bf1cadbfa70c9f7d13e34d88949d56d91b2365dd
kyler-harrison <harri828@purdue.edu>
2021-10-19 23:31:29 -0400

    testing nodeAST type

--- Parser.hs
+++ Parser.hs
@@ -3,9 +3,10 @@ module Parser where
 import Types (TokenSymbol (..), Token (..), NodeAST (..))
 
 {- TODO
-   [] create ASTToken type
-   [] map TokenSymbols in list of Tokens from scanner to ASTTokens with NodeOps (or maybe just use the same symbols as the scanner and skip these first two steps? not sure if things will become unclear later
-   [] create grammar to check against
-   [] function to build tree from ASTTokens (or just Tokens if you go that way)
+   [] function to build tree from Tokens using grammar 
    [] function to evaluate tree
 -}
+
+-- build abstract syntax tree from Tokens (from scanner), returns top node
+buildAST :: [Token] -> NodeAST
+buildAST tokenLs = NodeAST {nodeOp = PLUS, nodeIntVal = 0, left = NO_NODE, right = NO_NODE}
