81073ad475d84e08afc94b7991237b9e99ea55bc
kyler-harrison <harri828@purdue.edu>
2021-10-19 23:32:27 -0400

    added base case data type NO_NODE to NodeAST data type

--- Types.hs
+++ Types.hs
@@ -2,4 +2,4 @@ module Types (TokenSymbol (..), Token (..), NodeAST (..)) where
 
 data TokenSymbol = PLUS | MINUS | STAR | SLASH | INT_LIT | NONE deriving (Show)
 data Token = Token {tokenVal :: TokenSymbol, tokenIntVal :: Int} deriving (Show)
-data NodeAST = NodeAST {nodeOp :: TokenSymbol, nodeIntVal :: Int, left :: NodeAST, right :: NodeAST}
+data NodeAST = NodeAST {nodeOp :: TokenSymbol, nodeIntVal :: Int, left :: NodeAST, right :: NodeAST} | NO_NODE deriving (Show)
