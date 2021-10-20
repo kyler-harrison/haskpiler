8cccfbac5e0a5dd3abb4043c9a91cc7852010486
kyler-harrison <harri828@purdue.edu>
2021-10-19 23:18:33 -0400

    renamed operator to nodeOp and moved nodeIntVal up for no reason

--- Types.hs
+++ Types.hs
@@ -2,4 +2,4 @@ module Types (TokenSymbol (..), Token (..), NodeAST (..)) where
 
 data TokenSymbol = PLUS | MINUS | STAR | SLASH | INT_LIT | NONE deriving (Show)
 data Token = Token {tokenVal :: TokenSymbol, tokenIntVal :: Int} deriving (Show)
-data NodeAST = NodeAST {operator :: Token, left :: NodeAST, right :: NodeAST, nodeIntVal :: Int}
+data NodeAST = NodeAST {nodeOp :: Token, nodeIntVal :: Int, left :: NodeAST, right :: NodeAST}
