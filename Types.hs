feb9e4ce74bde474c97046b3bb1e315e572bca2a
kyler-harrison <harri828@purdue.edu>
2021-10-20 00:41:06 -0400

    changed NONE to NO_SYMBOL in TokenSymbol type, derived Eq also

--- Types.hs
+++ Types.hs
@@ -1,5 +1,5 @@
 module Types (TokenSymbol (..), Token (..), NodeAST (..)) where
 
-data TokenSymbol = PLUS | MINUS | STAR | SLASH | INT_LIT | NONE deriving (Show)
+data TokenSymbol = PLUS | MINUS | STAR | SLASH | INT_LIT | NO_SYMBOL deriving (Eq, Show)
 data Token = Token {tokenVal :: TokenSymbol, tokenIntVal :: Int} deriving (Show)
 data NodeAST = NodeAST {nodeOp :: TokenSymbol, nodeIntVal :: Int, left :: NodeAST, right :: NodeAST} | NO_NODE deriving (Show)
