240675fd302b5faa78ef455945c0491564f4ef95
kyler-harrison <harri828@purdue.edu>
2021-10-20 00:40:27 -0400

    boolean grammar check that appears to be working, will probably combine with buildAST eventually

--- Parser.hs
+++ Parser.hs
@@ -2,11 +2,28 @@ module Parser where
 
 import Types (TokenSymbol (..), Token (..), NodeAST (..))
 
-{- TODO
-   [] function to build tree from Tokens using grammar 
-   [] function to evaluate tree
--}
+-- TODO
+   --[] function to build tree from Tokens using grammar 
+   --[] function to evaluate tree
 
 -- build abstract syntax tree from Tokens (from scanner), returns top node
+{-
 buildAST :: [Token] -> NodeAST
-buildAST tokenLs = NodeAST {nodeOp = PLUS, nodeIntVal = 0, left = NO_NODE, right = NO_NODE}
+buildAST [] = []
+buildAST (token:tokenLs) 
+  | tokenVal token == INT_LIT
+
+buildAST tokenList = NodeAST {nodeOp = PLUS, nodeIntVal = 4, left = NodeAST {nodeOp = PLUS, nodeIntVal = 0, left = NO_NODE, right = NO_NODE}, right = NodeAST {nodeOp = PLUS, nodeIntVal = 0, left = NO_NODE, right = NO_NODE}}
+-}
+
+-- checks that Tokens read in from scanner (in read order) conform to the
+-- grammar of int op int (should define more generically and might combine
+-- with the actual construction of the AST)
+grammarCheck :: Token -> [Token] -> Bool
+grammarCheck prevToken [] = True
+grammarCheck prevToken (token:tokenLs)
+  | tokenVal prevToken == NO_SYMBOL = grammarCheck token tokenLs
+  | tokenVal token == INT_LIT && tokenVal prevToken /= INT_LIT = grammarCheck token tokenLs
+  | tokenVal token /= INT_LIT && tokenVal prevToken == INT_LIT = grammarCheck token tokenLs
+  | otherwise = False
+
