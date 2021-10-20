7206a699d28d2b32ac9d952a66245a6b36eca1c2
kyler-harrison <harri828@purdue.edu>
2021-10-19 23:09:22 -0400

    parser module

---
+++ Parser.hs
@@ -0,0 +1,11 @@
+module Parser where
+
+import Types (TokenSymbol (..), Token (..), NodeAST (..))
+
+{- TODO
+   [] create ASTToken type
+   [] map TokenSymbols in list of Tokens from scanner to ASTTokens with NodeOps (or maybe just use the same symbols as the scanner and skip these first two steps? not sure if things will become unclear later
+   [] create grammar to check against
+   [] function to build tree from ASTTokens (or just Tokens if you go that way)
+   [] function to evaluate tree
+-}
