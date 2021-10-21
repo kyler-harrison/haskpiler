b1bc8c10a50934c555d4a5223e33786f883e5710
kyler-harrison <harri828@purdue.edu>
2021-10-20 23:04:15 -0400

    update

--- main.hs
+++ main.hs
@@ -4,12 +4,11 @@ import Scan
 import Parser
 
 main = do
-    file <- openFile "valid.txt" ReadMode
+    file <- openFile "simple.txt" ReadMode
     contents <- hGetContents file
     let tokens = readTokens contents
-    print tokens
     let check = grammarCheck Token {tokenVal = NO_SYMBOL, tokenIntVal = 0}  tokens 
-    --let output = buildAST (readTokens contents)
-    print check
+    let tree = buildAST NO_NODE tokens
+    print tree
     hClose file
 
