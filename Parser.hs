43246925d0d2be9200d80b310e2d9cb6df62cf85
kyler-harrison <harri828@purdue.edu>
2021-10-26 23:16:45 -0400

    order

--- Parser.hs
+++ Parser.hs
@@ -3,15 +3,8 @@ module Parser where
 import Types (TokenSymbol (..), Token (..), NodeAST (..))
 
 -- TODO
-   --[] function to build tree from Tokens using grammar 
+   --[x] function to build tree from Tokens using grammar 
    --[] function to evaluate tree
-
--- build abstract syntax tree from Tokens (from scanner), assumes input has been checked for grammar, returns top node
-buildAST :: NodeAST -> [Token] -> NodeAST
-buildAST prevNode [] = prevNode
-buildAST prevNode (token:tokenLs) 
-  | tokenVal token == INT_LIT = buildAST NodeAST {nodeOp = tokenVal token, nodeIntVal = tokenIntVal token, left = NO_NODE, right = NO_NODE} tokenLs
-  | tokenVal token /= INT_LIT = NodeAST {nodeOp = tokenVal token, nodeIntVal = 0, left = prevNode, right = buildAST NO_NODE tokenLs}  -- TODO should handle each operator with its own guard instead of just "not int"
 
 -- checks that Tokens read in from scanner (in read order) conform to the
 -- grammar of int op int (should define more generically and might combine
@@ -23,4 +16,12 @@ grammarCheck prevToken (token:tokenLs)
   | tokenVal token == INT_LIT && tokenVal prevToken /= INT_LIT = grammarCheck token tokenLs
   | tokenVal token /= INT_LIT && tokenVal prevToken == INT_LIT = grammarCheck token tokenLs
   | otherwise = False
+
+-- build abstract syntax tree from Tokens (from scanner), assumes input has been checked for grammar, returns top node
+buildAST :: NodeAST -> [Token] -> NodeAST
+buildAST prevNode [] = prevNode
+buildAST prevNode (token:tokenLs) 
+  | tokenVal token == INT_LIT = buildAST NodeAST {nodeOp = tokenVal token, nodeIntVal = tokenIntVal token, left = NO_NODE, right = NO_NODE} tokenLs
+  | tokenVal token /= INT_LIT = NodeAST {nodeOp = tokenVal token, nodeIntVal = 0, left = prevNode, right = buildAST NO_NODE tokenLs}  -- TODO should handle each operator with its own guard instead of just "not int"
+
 
